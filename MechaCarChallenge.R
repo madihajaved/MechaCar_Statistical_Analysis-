17cf21b5bea9d2f800499cb59b3bc1a144f8a5c6
madihajaved <madiha.jave@gmail.com>
2021-07-03 16:08:25 -0400

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -27,3 +27,18 @@ total_summary <- susp_coil %>% summarize(Mean=mean(PSI),Median=median(PSI), Vari
 lot_summary <- susp_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep') 
 View(lot_summary)
 
+# t-test 
+t.test(x=susp_coil$PSI, mu=1500)
+
+#t-test for each Lot 
+lot1 <- subset(susp_coil, Manufacturing_Lot=='Lot1')
+t.test(x=lot1$PSI, mu=1500)
+
+lot2 <- subset(susp_coil, Manufacturing_Lot=='Lot2')
+t.test(x=lot2$PSI, mu=1500)
+
+lot3 <- subset(susp_coil, Manufacturing_Lot=='Lot3')
+t.test(x=lot3$PSI, mu=1500)
+
+
+
